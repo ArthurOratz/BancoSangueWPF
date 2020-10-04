f5c6801f5fba90cf8732dd65351e61d988149628
Arthur Oratz <arthur_oratz@hotmail.com>
2020-10-04 15:59:43 -0300

    versao .net

--- BancoSangueWPF/obj/Debug/netcoreapp3.1/Views/frmCadastrarDoador.g.i.cs
+++ BancoSangueWPF/obj/Debug/netcoreapp3.1/Views/frmCadastrarDoador.g.i.cs
@@ -77,7 +77,7 @@ namespace BancoSangueWPF.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/BancoSangueWPF;V1.0.0.0;component/views/frmcadastrardoador.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/BancoSangueWPF;component/views/frmcadastrardoador.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\frmCadastrarDoador.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
