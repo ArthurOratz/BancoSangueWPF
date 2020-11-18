c6324556bc6219d3fdc190a40261242823134140
Arthur Oratz <arthur_oratz@hotmail.com>
2020-11-18 00:38:15 -0300

    criação BancoSangueWeb

---
+++ BancoDeSangueWeb/Controllers/HospitalController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BancoDeSangueWeb.Controllers
+{
+    public class HospitalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar() => View();
+        [HttpPost]
+        public IActionResult Cadastrar(string Nome, string Endereco, string Responsavel)
+        {
+            return RedirectToAction("Index", "Hospital");
+        }
+
+
+
+
+    }
+}
